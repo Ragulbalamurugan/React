aa33c45cebd76824a6560f8901a5206f987bc9a8
Ragulbalamurugan <ragulbrs70871@gmail.com>
2022-04-30 14:27:44 +0530

    second commit

--- src/index.js
+++ src/index.js
@@ -1,23 +1,11 @@
-// Reacthooks
-import React,{useEffect, useState} from 'react';
+import React from 'react'
 import ReactDOM from 'react-dom';
 
-function ReactHooks(){
-  const[count, updatecount] = useState(0);
-  useEffect(()=>{
-    alert('hello reactj')
-  })
-  return(
-    <div>
-     
-      <button onClick={()=>updatecount(count + 1)}>
-        click the button
-      </button> <p>
-        you clicked the above button {count} times
-      </p>
-
-    </div>
-  )
+function Header(){
+    return(<div>
+        <h1>
+            Reactjs
+            </h1>
+        </div>)
 }
- 
-ReactDOM.render(<ReactHooks/>, document.getElementById('root'))
+ReactDOM.render(<Header/>,document.getElementById('root'))
