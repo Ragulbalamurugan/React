7dbea858af04433db45c5a5ca5627b584949dc05
Ragulbalamurugan <ragulbrs70871@gmail.com>
2022-05-01 07:21:12 +0530

    new commit

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom';
 
 function Header(){
-    return(<div>
-        <h1>
-            Reactjs tutorial for beginners
+    return(
+        <div>
+            <h1>
+                react is library in javascript which
+                was created by faccebook and it is 
+                runing successful today
             </h1>
-        </div>)
+        </div>
+    )
 }
 ReactDOM.render(<Header/>,document.getElementById('root'))
